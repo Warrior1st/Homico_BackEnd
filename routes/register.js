f555c109ce4e388098c4af01ff29b7cda36b5a3d
Warrior1st <61919684+Warrior1st@users.noreply.github.com>
2023-06-30 17:54:55 -0400

    Debug the web server

--- routes/register.js
+++ routes/register.js
@@ -1,36 +1,36 @@
 var express = require("express");
 var router = express.Router();
-const odbc = require("odbc");
+//const odbc = require("odbc");
 
-// router.get("/", function (req, res, next) {
-//   res.render("register", { title: "Cooking" });
-// });
+router.get("/", function (req, res, next) {
+  res.render("register", { title: "Cooking" });
+});
 
 /* GET users listing. */
-router.post("/", async (req, res, next) => {
-  const { firstName, lastName, email, username, password } = req.body;
-  const userRole = 2;
+// router.post("/", async (req, res, next) => {
+//   const { firstName, lastName, email, username, password } = req.body;
+//   const userRole = 2;
 
-  try {
-    // Establish a connection to the database using the ODBC driver and connection string
-    //const connectionString = process.env.dbconnection;
-    const connectionString =
-      "Driver={ODBC Driver 18 for SQL Server};Server=tcp:homicoserver.database.windows.net,1433;Database=homico;Uid=homico_admin;Pwd={Nejamaistrahir1997};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;";
-    const connection = await odbc.connect(connectionString);
+//   try {
+//     // Establish a connection to the database using the ODBC driver and connection string
+//     //const connectionString = process.env.dbconnection;
+//     const connectionString =
+//       "Driver={ODBC Driver 18 for SQL Server};Server=tcp:homicoserver.database.windows.net,1433;Database=homico;Uid=homico_admin;Pwd={Nejamaistrahir1997};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;";
+//     const connection = await odbc.connect(connectionString);
 
-    //Insert new user in the database
-    await connection.query(
-      "insert into users(firstName, lastName, email, userName, passwrd, roleId) values (?,?,?,?,?,?)",
-      [firstName, lastName, email, username, password, userRole]
-    );
+//     //Insert new user in the database
+//     await connection.query(
+//       "insert into users(firstName, lastName, email, userName, passwrd, roleId) values (?,?,?,?,?,?)",
+//       [firstName, lastName, email, username, password, userRole]
+//     );
 
-    return res.sendStatus(204);
-  } catch (error) {
-    console.error("Database query error:", error);
-    return res
-      .status(500)
-      .json({ error: "An error occurred during the database query" });
-  }
-});
+//     return res.sendStatus(204);
+//   } catch (error) {
+//     console.error("Database query error:", error);
+//     return res
+//       .status(500)
+//       .json({ error: "An error occurred during the database query" });
+//   }
+// });
 
 module.exports = router;
