f854b157875ce5a07c5ccc76c324917c431f87e6
Warrior1st <61919684+Warrior1st@users.noreply.github.com>
2023-09-09 16:28:53 -0400

    Not done the implementation of the image uploader, need to test it

---
+++ public/javascripts/propertyScript.js
@@ -0,0 +1,162 @@
+// JavaScript code in your Pug template or external JavaScript file
+document.addEventListener("DOMContentLoaded", function () {
+  const editButtons = document.querySelectorAll("a.edit");
+
+  editButtons.forEach((editButton) => {
+    editButton.addEventListener("click", function (event) {
+      event.preventDefault();
+
+      const propertyId = this.getAttribute("data-property-id");
+      // You now have the 'propertyId' associated with the clicked row
+
+      // Use 'propertyId' to look up the property data in your dictionary
+      makeAjaxRequest(propertyId);
+    });
+  });
+});
+
+function populateEditModal(response) {
+  const modal = document.querySelector("#editEmployeeModal");
+  var property = response.prop;
+  var agencies = response.agency;
+  var cities = response.city;
+  var propType = response.propType;
+
+  //Refactor the image sources
+  const imageIdsArray = property[0].images.split(";");
+
+  // Initialize an array to store image objects
+  const existingImages = [];
+
+  // For each ID in the array, construct the image URL and add it to the array
+  let i = 0;
+  imageIdsArray.forEach((imageId) => {
+    // Assuming your images are stored in a specific directory, adjust the path accordingly
+    const imageUrl = `https://homico.azurewebsites.net/images/${imageId}.jpg`;
+
+    // Add the image object to the array
+    existingImages.push({
+      id: i, // You can keep the original image ID
+      url: imageUrl, // Constructed image URL
+    });
+    i++;
+  });
+
+  appendExistingImages(existingImages);
+
+  // Access the form elements within the modal
+  const propertyIdField = modal.querySelector('input[name="propertyId"]');
+  const nomInput = modal.querySelector('input[name="nom"]');
+  const descriptionTextarea = modal.querySelector(
+    'textarea[name="description"]'
+  );
+  const adresseInput = modal.querySelector('input[name="adresse"]');
+  const chambresInput = modal.querySelector('input[name="chambres"]');
+  const bainsInput = modal.querySelector('input[name="bains"]');
+  const cuisinesInput = modal.querySelector('input[name="cuisines"]');
+  const parkingsInput = modal.querySelector('input[name="parkings"]');
+  const prixInput = modal.querySelector('input[name="prix"]');
+  const latitudeInput = modal.querySelector('input[name="latitude"]');
+  const longitudeInput = modal.querySelector('input[name="longitude"]');
+  const tailleInput = modal.querySelector('input[name="taille"]');
+  const typeVenteSelect = modal.querySelector('input[name="typeVente"]');
+  const typeProprieteSelect = modal.querySelector(
+    'select[name="typePropriete"]'
+  );
+  const villeSelect = modal.querySelector('select[name="ville"]');
+  const agenceSelect = modal.querySelector('select[name="agence"]');
+
+  // Set the values of the form elements based on the property object
+  propertyIdField.value = property[0].propertyId;
+  nomInput.value = property[0].title;
+  descriptionTextarea.value = property[0].descr;
+  adresseInput.value = property[0].addres;
+  chambresInput.value = property[0].beds;
+  bainsInput.value = property[0].baths;
+  cuisinesInput.value = property[0].kitchens;
+  parkingsInput.value = property[0].parkings;
+  prixInput.value = property[0].price;
+  latitudeInput.value = property[0].latitude;
+  longitudeInput.value = property[0].longitude;
+  tailleInput.value = property[0].size;
+
+  // Set the selected option in the selects based on the property object
+  typeVenteSelect.value = property[0].saleType;
+
+  propType.forEach(function (value) {
+    let option = document.createElement("option");
+    option.text = value.nom;
+    option.value = value.propertyTypeId;
+
+    if (value === property[0].propertyType) {
+      option.selected = true;
+    }
+    typeProprieteSelect.appendChild(option);
+  });
+
+  cities.forEach(function (value) {
+    let option = document.createElement("option");
+    option.text = value.nom;
+    option.value = value.cityId;
+
+    if (value === property[0].city) {
+      option.selected = true;
+    }
+
+    villeSelect.appendChild(option);
+  });
+
+  agencies.forEach(function (value) {
+    let option = document.createElement("option");
+    option.text = value.name;
+    option.value = value.agenceId;
+
+    if (value === property[0].agence) {
+      option.selected = true;
+    }
+
+    agenceSelect.appendChild(option);
+  });
+
+  $("#editEmployeeModal").modal("show");
+}
+
+// Define a function to append existing images
+function appendExistingImages(existingImages) {
+  const container = document.getElementById("existing-images-container");
+
+  existingImages.forEach((image) => {
+    const existingImageDiv = document.createElement("div");
+    existingImageDiv.classList.add("existing-image");
+
+    const imgElement = document.createElement("img");
+    imgElement.src = image.url;
+    imgElement.alt = "Existing Image";
+
+    const deleteButton = document.createElement("button");
+    deleteButton.classList.add("delete-image");
+    deleteButton.type = "button";
+    deleteButton.dataset.imageId = image.id;
+    deleteButton.textContent = "Supprimer";
+
+    existingImageDiv.appendChild(imgElement);
+    existingImageDiv.appendChild(deleteButton);
+
+    container.appendChild(existingImageDiv);
+  });
+}
+
+function makeAjaxRequest(propId) {
+  // Make an AJAX request to fetch options based on propertyTypeId
+  $.ajax({
+    url: "/property/mngmntPropertyType", // Replace with the actual route to fetch options
+    method: "POST",
+    data: { propId: propId },
+    success: function (response) {
+      populateEditModal(response);
+    },
+    error: function (error) {
+      console.error("Error fetching options:", error);
+    },
+  });
+}
--- routes/adminProperty.js
+++ routes/adminProperty.js
@@ -11,15 +11,249 @@ const config = {
     encrypt: true,
   },
 };
+router.post("/mngmntPropertyType", function (req, res, next) {
+  if (req.session.loggedIn) {
+    const propId = req.body.propId;
+    const query = `
+  select [Property].[propertyId] as propertyId, [City].[nom] as city, [title], [descr], [Property].[images], [beds], [baths],
+  [kitchens], [parkings], [price], [addres], [Property].[latitude], [Property].[longitude], [datePosted], [size], [saleType],
+  [PropertyType].[nom] as propertyType, [Agences].[name] as agence
+  from property 
+  left join city on property.cityId = City.cityId
+  left join PropertyType on Property.propertyTypeId = PropertyType.propertyTypeId
+  left join Agences ON Property.agenceId = Agences.agenceId
+  where [Property].[propertyId] = ${propId};
+  
+  select [propertyTypeId], [nom] from PropertyType;
+  select [cityId], [nom] from City;
+  select [agenceId], [name] from Agences;
+`;
+
+    sql.connect(config, function (err) {
+      if (err) {
+        console.log(err);
+        return;
+      }
+
+      const request = new sql.Request();
+      request.query(query, function (err, result) {
+        if (err) {
+          console.log(err);
+          return;
+        }
+
+        // Extract the data from the SQL result
+        const [properties, propTypes, cities, agences] = result.recordsets;
+
+        let adminPropMngmnt = {
+          prop: properties,
+          propType: propTypes,
+          city: cities,
+          agency: agences,
+        };
+
+        // Build the adminPropMngmnt model
+        return res.json(adminPropMngmnt);
+      });
+    });
+  } else {
+    return res.status(403).json("Not authorized");
+  }
+});
 
 router.get("/", function (req, res, next) {
   if (req.session.loggedIn) {
     // User is logged in
-    res.render("property", { title: "Property Management" });
+    if (Object.keys(req.query).length > 0) {
+      // There are query parameters, execute the update query
+      const {
+        propertyId,
+        nom,
+        description,
+        adresse,
+        chambres,
+        bains,
+        cuisines,
+        parkings,
+        prix,
+        latitude,
+        longitude,
+        taille,
+        typeVente,
+        typePropriete,
+        ville,
+        agence,
+      } = req.query;
+
+      // Construct and execute the update query here
+      const updateQuery = `
+        UPDATE Property SET title = ${nom},
+        descr = ${description},
+        addres = ${adresse},
+        beds = ${chambres},
+        baths = ${bains},
+        kitchens = ${cuisines},
+        parkings = ${parkings},
+        price = ${prix},
+        latitude = ${latitude},
+        longitude = ${longitude},
+        size = ${taille},
+        saleType = ${typeVente},
+        propertyTypeId = ${typePropriete},
+        cityId = ${ville},
+        agenceId = ${agence} WHERE propertyId = ${propertyId};
+      `;
+
+      sql.connect(config, function (err) {
+        if (err) {
+          console.log(err);
+          return;
+        }
+
+        const request = new sql.Request();
+        request.query(updateQuery, function (err, result) {
+          if (err) {
+            console.log(err);
+            return;
+          }
+
+          // Handle the update result if needed
+
+          // Now execute the select queries
+          executeSelectQueries(req, res);
+        });
+      });
+    } else {
+      // No query parameters, execute the select queries directly
+      executeSelectQueries(req, res);
+    }
   } else {
-    // User is not logged in, redirect to login
     res.redirect("/login");
   }
 });
+
+function executeSelectQueries(req, res) {
+  // Execute the select queries here
+  const query = `
+    select [Property].[propertyId] as propertyId, [City].[nom] as city, [title], [descr], [Property].[images] as images, [beds], [baths],
+    [kitchens], [parkings], [price], [addres], [Property].[latitude], [Property].[longitude], [datePosted], [size], [saleType], [PropertyType].[nom] as propertyType, [Agences].[name] as agence
+    FROM property
+    LEFT JOIN city ON property.cityId = City.cityId
+    LEFT JOIN PropertyType ON Property.propertyTypeId = PropertyType.propertyTypeId
+    LEFT JOIN Agences ON Property.agenceId = Agences.agenceId;
+    
+    SELECT [cityId],[nom] FROM City;
+    SELECT [propertyTypeId], [nom] FROM PropertyType;
+    SELECT [agenceId], [name] FROM Agences;
+  `;
+
+  sql.connect(config, function (err) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+
+    const request = new sql.Request();
+    request.query(query, function (err, result) {
+      if (err) {
+        console.log(err);
+        return;
+      }
+
+      // Extract the data from the SQL result
+      const [properties, cities, propertyTypes, agences] = result.recordsets;
+      // Build the dashboard model
+
+      const propertyDictionary = {};
+      properties.forEach((property) => {
+        propertyDictionary[property.propertyId] = property;
+      });
+
+      const citiesDictionary = {};
+      cities.forEach((city) => {
+        citiesDictionary[city.cityId] = city.nom;
+      });
+
+      const agencesDictionary = {};
+      agences.forEach((agence) => {
+        agencesDictionary[agence.agenceId] = agence.name;
+      });
+
+      res.render("property", {
+        title: "Property Management",
+        houses: propertyDictionary,
+        cities: citiesDictionary,
+        propertyTypes: propertyTypes,
+        agences: agencesDictionary,
+      });
+    });
+  });
+}
+
+// router.get("/", function (req, res, next) {
+//   if (req.session.loggedIn) {
+//     // User is logged in
+//     //Retrieve the houses data from the database
+//     //TODO: Absolutely need to implement paging for this
+
+//     // Check if the role is "user"
+//     // Fetch the data for the dashboard
+//     // Modify the SQL query to retrieve the required data for the dashboard
+//     const query = `
+//     select [Property].[propertyId] as propertyId, [City].[nom] as city,[title], [descr],[Property].[images], [beds], [baths],
+//     [kitchens], [parkings], [price], [addres], [Property].[latitude], [Property].[longitude], [datePosted], [size], [saleType], [PropertyType].[nom] as propertyType, [Agences].[name] as agence from property left join city on property.cityId = City.cityId left join PropertyType on Property.propertyTypeId = PropertyType.propertyTypeId left join Agences ON Property.agenceId = Agences.agenceId;
+
+//     select [cityId],[nom] from City;
+//     select [propertyTypeId], [nom] from PropertyType;
+//     select [agenceId], [name] from Agences;
+
+//  `;
+
+//     sql.connect(config, function (err) {
+//       if (err) {
+//         console.log(err);
+//         return;
+//       }
+
+//       const request = new sql.Request();
+//       request.query(query, function (err, result) {
+//         if (err) {
+//           console.log(err);
+//           return;
+//         }
+
+//         // Extract the data from the SQL result
+//         const [properties, cities, propertyTypes, agences] = result.recordsets;
+//         // Build the dashboard model
+
+//         const propertyDictionary = {};
+//         properties.forEach((property) => {
+//           propertyDictionary[property.propertyId] = property;
+//         });
+
+//         const citiesDictionary = {};
+//         cities.forEach((city) => {
+//           citiesDictionary[city.cityId] = city.nom;
+//         });
+
+//         const agencesDictionary = {};
+//         agences.forEach((agence) => {
+//           agencesDictionary[agence.agenceId] = agence.name;
+//         });
+
+//         res.render("property", {
+//           title: "Property Management",
+
+//           houses: propertyDictionary,
+//           cities: citiesDictionary,
+//           propertyTypes: propertyDictionary,
+//           agences: agencesDictionary,
+//         });
+//       });
+//     });
+//   } else {
+//     res.redirect("/login");
+//   }
+// });
 
 module.exports = router;
